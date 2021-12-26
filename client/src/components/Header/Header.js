d104c7a6296aa23111b5310c359b2f3da5ac5302
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-26 14:26:10 +0200

    add data json and words js

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import "../../css/Header/Header.css"
 import React from "react";
+import { words } from "../../words";
+
 
 function Header (){
 
     return (
-    <header>Shopping Cart</header>
+    <header>{words.headerTitle}</header>
     )
 }
 
