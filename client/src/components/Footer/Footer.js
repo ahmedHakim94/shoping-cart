8a8daaa7537d42db7585af75f98cee75abf9186b
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-26 11:18:14 +0200

    starter react project

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../css/Footer/Footer.css"
+
+
+function Footer (){
+    return(
+        <footer>footer</footer>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "../../css/Header/Header.css"
+import React from "react";
+
+function Header (){
+
+    return (
+    <header>Shopping Cart</header>
+    )
+}
+
+export default Header;
