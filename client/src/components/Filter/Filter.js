947844c788712452c3baa41ded8e133f273e25f6
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-27 12:48:32 +0200

    filter comp

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import '../../css/Filter/Filter.css'
-function Filter() {
+function Filter(props) {
     return (
         <div className='filter'>
             <h2>filter</h2>
             <p>Number Of Product : 4</p>
             <div className='filter-by-size'>
                 <span>Size</span>
-                <select class="form-control">
-                    <option value="all">All</option>
-                    <option value="m">M</option>
-                    <option value="l">L</option>
-                    <option value="xl">Xl</option>
-                    <option value="xxl">XXl</option>
+                <select onChange={props.handelFilterBySize} value={props.size} className="form-control">
+                    <option value="all">all</option>
+                    <option value="m">m</option>
+                    <option value="l">l</option>
+                    <option value="xl">xl</option>
+                    <option value="xxl">xxl</option>
                 </select>
             </div>
 
             <div className='filter-by-order'>
                 <span>Order</span>
-                <select class="form-control">
-                    <option value="latest">Latest</option>
-                    <option value="lower">Lower</option>
-                    <option value="height">Height</option>
+                <select onChange={props.handelFilterByOrder} value={props.order} className="form-control">
+                    <option value="latest">latest</option>
+                    <option value="lower">lower</option>
+                    <option value="height">height</option>
 
                 </select>
             </div>
