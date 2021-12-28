470f4ce9f964ca4f35c4dc084f49a69482be8db9
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-28 10:52:30 +0200

    animation

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -2,26 +2,31 @@ import React, { useState } from 'react'
 import "../../css/Cart/Cart.css"
 import CheckoutForm from '../CheckoutForm/CheckoutForm'
 
+import Slide  from 'react-reveal/Slide'
+
+
 
 function Cart(props) {
 
-    const[checkoutForm,setCheckoutForm] = useState(false)
+    const [checkoutForm, setCheckoutForm] = useState(false)
 
-    const[value,setValue] = useState("")
-    
-    const handelForm = (e)=>{
+    const [value, setValue] = useState("")
+
+    const handelForm = (e) => {
         e.preventDefault();
         const order = {
-            name:value.name,
-            email:value.email}
+            name: value.name,
+            email: value.email
+        }
         console.log(order)
 
     }
-    const handelinput =(e)=>{
-        setValue( (prevstate)=>({...prevstate,[e.target.name]:e.target.value}) )
+    const handelinput = (e) => {
+        setValue((prevstate) => ({ ...prevstate, [e.target.name]: e.target.value }))
     }
 
     return (
+        
         <div className='cart'>
             <div className='cart-title'>{props.cartitems.length === 0 ? <span>Empty Cart</span> : <p>
                 there is {props.cartitems.length} product in cart
@@ -30,6 +35,7 @@ function Cart(props) {
             </div>
             {props.cartitems.map(item => {
                 return (
+                    <Slide right >
                     <div key={item.id} className='cart-items'>
                         <img src={item.imageurl} />
                         <div className='item-desc'>
@@ -43,18 +49,19 @@ function Cart(props) {
 
 
                     </div>
+                    </Slide>
                 )
             })}
             {props.cartitems.length !== 0 &&
                 <div className='cart-footer'>
-                    <div className='total'>Total : ${props.cartitems.reduce( (acc,p) =>{
-                        return acc+(p.qty * p.price)
-                    },0)}</div>
-                    <button onClick={()=>setCheckoutForm(true)} className='btn btn-primary'>Select Product</button>
+                    <div className='total'>Total : ${props.cartitems.reduce((acc, p) => {
+                        return acc + (p.qty * p.price)
+                    }, 0)}</div>
+                    <button onClick={() => setCheckoutForm(true)} className='btn btn-primary'>Select Product</button>
                 </div>
             }
 
-            <CheckoutForm handelinput={handelinput} handelForm={handelForm} checkoutForm={checkoutForm} setCheckoutForm={setCheckoutForm}/>
+            <CheckoutForm handelinput={handelinput} handelForm={handelForm} checkoutForm={checkoutForm} setCheckoutForm={setCheckoutForm} />
 
 
         </div>
