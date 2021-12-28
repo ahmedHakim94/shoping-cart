e5bc88224c411a69687c0f5b6bed8d1a78b6a96b
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-28 10:07:46 +0200

    checkout form

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,30 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../../css/Cart/Cart.css"
+import CheckoutForm from '../CheckoutForm/CheckoutForm'
+
+
 function Cart(props) {
+
+    const[checkoutForm,setCheckoutForm] = useState(false)
+
+    const[value,setValue] = useState("")
+    
+    const handelForm = (e)=>{
+        e.preventDefault();
+        const order = {
+            name:value.name,
+            email:value.email}
+        console.log(order)
+
+    }
+    const handelinput =(e)=>{
+        setValue( (prevstate)=>({...prevstate,[e.target.name]:e.target.value}) )
+    }
+
     return (
         <div className='cart'>
-            <div className='cart-title'>{props.cartitems.length ===0 ? <span>Empty Cart</span> : <p>
-               there is {props.cartitems.length} product in cart
-                </p>
-                }
-                </div>
+            <div className='cart-title'>{props.cartitems.length === 0 ? <span>Empty Cart</span> : <p>
+                there is {props.cartitems.length} product in cart
+            </p>
+            }
+            </div>
             {props.cartitems.map(item => {
                 return (
                     <div key={item.id} className='cart-items'>
                         <img src={item.imageurl} />
                         <div className='item-desc'>
                             <div className='item-info'>
-                                <p>{item.title}</p>
-                                <p>qty{item.qty}</p>
-                                <p>{item.price}</p>
+                                <p>Title :{item.title}</p>
+                                <p>quantity :{item.qty}</p>
+                                <p>Price :${item.qty * item.price}</p>
                             </div>
-                            <button onClick={() =>props.removeItem(item)} className='btn btn-primary'>Remove</button>
+                            <button onClick={() => props.removeItem(item)} className='btn btn-primary'>Remove</button>
                         </div>
 
 
                     </div>
                 )
             })}
+            {props.cartitems.length !== 0 &&
+                <div className='cart-footer'>
+                    <div className='total'>Total : ${props.cartitems.reduce( (acc,p) =>{
+                        return acc+(p.qty * p.price)
+                    },0)}</div>
+                    <button onClick={()=>setCheckoutForm(true)} className='btn btn-primary'>Select Product</button>
+                </div>
+            }
+
+            <CheckoutForm handelinput={handelinput} handelForm={handelForm} checkoutForm={checkoutForm} setCheckoutForm={setCheckoutForm}/>
 
 
         </div>
