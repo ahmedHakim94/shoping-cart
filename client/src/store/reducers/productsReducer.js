69095ea21e5c0c7d7dec12f264fdb67e33b74eda
Ahmed Abdel Hakim <Ahmed.Hakim@Intellisec-me.com>
2021-12-30 09:31:14 +0200

    redux-filter

--- client/src/store/reducers/productsReducer.js
+++ client/src/store/reducers/productsReducer.js
@@ -1,9 +1,14 @@
-import { FETCH_PRODUCTS } from "../actions/type";
+import { FETCH_PRODUCTS, FILTER_SIZE, FILTER_SORT } from "../actions/type";
 import axios from 'axios';
+import Products from "../../components/Products/Products";
 
 const INITIAL_STATE = {
     products: [],
-    updateProducts: false
+    updateProducts: false,
+    size: "",
+    sort: "",
+    filterProduct: [],
+    updateFilterProducts: false
 }
 
 export const productsReducer = (state = INITIAL_STATE, action) => {
@@ -12,28 +17,88 @@ export const productsReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 products: action.data,
-                updateProducts: true
+                updateProducts: true,
+                filterProduct: action.data,
+            }
+        case FILTER_SIZE:
+            return {
+                ...state,
+                size: action.data.size,
+                filterProduct: action.data.product,
+            }
+
+        case FILTER_SORT:
+            return {
+                ...state,
+                sort: action.data.sort,
+                filterProduct: action.data.product,
             }
         default:
             return state
 
     }
 }
+export const filterdSize = (products, value) => {
+
+    return (dispatch) => {
+
+        const cloneProduct = [...products]
+        const newProduct = cloneProduct.filter(p => p.sizes.indexOf(value) != -1);
+        dispatch({
+            type: FILTER_SIZE,
+            data: {
+                size: value,
+                product: value == "all" ? products : newProduct
+            }
+        })
+    }
+}
+
+export const filterdOrder = (products, value) => {
+    debugger
+    return (dispatch) => {
+        let cloneProduct = [...products]
+        let newProduct = cloneProduct.sort( function (a, b) {
+            if (value == "lower") {
+                return a.price - b.price
+            }
+            else if (value == "height") {
+                return b.price - a.price
+            }
+            else {
+                return a.id < b.id ? 1 : -1
+            }
+        })
+    
+        dispatch({
+            type: FILTER_SORT,
+            data: {
+                sort: value,
+                product: newProduct
+            }
+        })
+    }
+}
+
+// export const fetchCarts = () => {
+
+//     return (dispatch) => {
+//         fetch('/api/products').then(res => res.json()).then(data => {
+//             dispatch({
+//                 type: FETCH_CARTS,
+//                 data: data
+//             })
+//         })
+//     }
+// }
 
 export const fetchProducts = () => {
-    
-    // const response = axios.get(`/api/products`);
 
-    // return {
-    //     type: FETCH_PRODUCTS,
-    //     payload: response
-    // }
-
-    return (dispatch) =>{
-        fetch('/api/products').then(res => res.json()).then(data =>{
+    return (dispatch) => {
+        fetch('/api/products').then(res => res.json()).then(data => {
             dispatch({
-                type:FETCH_PRODUCTS,
-                data:data
+                type: FETCH_PRODUCTS,
+                data: data
             })
         })
     }
